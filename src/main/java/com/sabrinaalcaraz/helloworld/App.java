dfae3c3d6d07eeaac42f7a9f8e4299b562de9a37
Nicolás Melendez <nfmelendez@gmail.com>
2016-07-11 21:13:11 -0300

    first commit of sabrina

---
+++ src/main/java/com/sabrinaalcaraz/helloworld/App.java
@@ -0,0 +1,15 @@
+package com.sabrinaalcaraz.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String name = "Sabrina";
+    	String out = "Hello World! " + name;
+        System.out.println(out);
+    }
+}
